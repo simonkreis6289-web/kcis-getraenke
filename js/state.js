3d91b7f3c91121c510ae61c716f943fb1a219909
simonkreis6289-web <simonkreis6289@gmail.com>
2026-05-22 10:53:32 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,5 +1,5 @@
 const APP_VERSION = '20260318-13';
-const APPVersionSelf = '2.0.0.08, © SK';
+const APPVersionSelf = '2.0.0.10, © SK';
 
 const LOCAL_STATE_KEY = 'kcis_local_state_v6';
 const FIRESTORE_ENABLED = true;
